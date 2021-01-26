1d4cfb13b3ef34d2fd76bc324ee3b98427bcf5e4
KatarzynaBrandys <68773576+KatarzynaBrandys@users.noreply.github.com>
2021-01-26 20:35:19 +0100

    update

--- jquery-zadania/5-menu-boczne-i-efekt-wyjscia/js/main.js
+++ jquery-zadania/5-menu-boczne-i-efekt-wyjscia/js/main.js
@@ -1,12 +1,11 @@
-$(document).ready(function() {
-    $('.hamburger').click(function(){
-        $('.main-menu').toggleClass('open');
-    });
-
-    $('.main-menu-item a').click(function(e){
+$(document).ready(function () {
+    $(".out-animation-wrapper").click(function () {
+        $(".main-menu").toggleClass("open")
+    })
+    $(".main-menu-item a").click(function (e) {
         e.preventDefault();
-        $('body').fadeOut(2000, function(){
-            window.location.href = $(e.target).attr('href');
+        $("body").fadeOut(2000, function () {
+            window.location.href = $(e.target).attr("href");
         })
     })
 });
